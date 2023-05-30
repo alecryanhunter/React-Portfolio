4053cde3d65e05b4fa26f152ea905c170615c93e
Alec Hunter <alecryanhunter@gmail.com>
2023-05-29 18:58:47 -0700

    added project/portfolio CSS

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,12 @@
 function Project(props) {
     return <section className="project">
-        <h3>{props.title}</h3>
-        <a href={props.github}>Github Link</a>
-        <a href={props.link}>Site Link</a>
+        <a href={props.link} target="_blank">
+        <img src={props.image}></img>
+        <section className="info">
+            <h3>{props.title}</h3>
+            <a href={props.github}>Github Link</a>
+        </section>
+        </a>
     </section>
 }
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,14 +3,44 @@ import Project from "../components/Project"
 function Portfolio() {
     return <main>
         <section className="portfolio page">
-            <h2 className="title">My Portfolio!</h2>
+            <h2 className="title">My Works</h2>
             <section className="portfolio-main">
-                <Project title="Campricorn" github="" link=""/>
-                <Project title="Quiz Game" github="" link=""/>
-                <Project title="Quiz Game" github="" link=""/>
-                <Project title="Quiz Game" github="" link=""/>
-                <Project title="Quiz Game" github="" link=""/>
-                <Project title="Quiz Game" github="" link=""/>
+                <Project
+                    title="Campricorn"
+                    github="https://github.com/aerostokes/camping-starcharts"
+                    link="https://aerostokes.github.io/camping-starcharts/"
+                    image="http://placekitten.com/700"
+                />
+                <Project 
+                    title="Quiz Game" 
+                    github="" 
+                    link="" 
+                    image="http://placekitten.com/700"
+                />
+                <Project 
+                    title="Quiz Game" 
+                    github="" 
+                    link="" 
+                    image="http://placekitten.com/700"
+                />
+                <Project 
+                    title="Quiz Game" 
+                    github="" 
+                    link="" 
+                    image="http://placekitten.com/700"
+                />
+                <Project 
+                    title="Quiz Game" 
+                    github="" 
+                    link="" 
+                    image="http://placekitten.com/700"
+                />
+                <Project 
+                    title="Quiz Game" 
+                    github="" 
+                    link="" 
+                    image="http://placekitten.com/700"
+                />
             </section>
         </section>
     </main>
