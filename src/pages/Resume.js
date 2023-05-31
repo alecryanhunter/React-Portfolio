2b955ebbfc83e358ac663b654f38224fd9ab7621
Alec Hunter <alecryanhunter@gmail.com>
2023-05-30 22:06:35 -0700

    added resume section

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,36 @@
 function Resume() {
     return <main>
         <section className="resume page">
-            <h2 className="title">My Resume!</h2>
+            <h2 className="title">My Resume</h2>
+            <section className="skills">
+                <h3>My Proficiences</h3>
+                <hr/>
+                <h4>HTML</h4>
+                <div className="bar">
+                    <div className="fill f-100"></div>
+                </div>
+                <h4>CSS</h4>
+                <div className="bar">
+                    <div className="fill f-80"></div>
+                </div>
+                <h4>JavaScript</h4>
+                <div className="bar">
+                    <div className="fill f-80"></div>
+                </div>
+                <h4>SQL</h4>
+                <div className="bar">
+                    <div className="fill f-80"></div>
+                </div>
+                <h4>Express</h4>
+                <div className="bar">
+                    <div className="fill f-60"></div>
+                </div>
+                <h4>React</h4>
+                <div className="bar">
+                    <div className="fill f-60"></div>
+                </div>
+            </section>
+            <a download href="resume.md">Download Resume</a>
         </section>
     </main>
 }
