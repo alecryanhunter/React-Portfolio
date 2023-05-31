e8166c5c4403f1e9d5282644b8b5cc42556fdddc
Alec Hunter <alecryanhunter@gmail.com>
2023-05-30 22:26:46 -0700

    finished portfolio with images and styling

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,37 +9,37 @@ function Portfolio() {
                     title="Campricorn"
                     github="https://github.com/aerostokes/camping-starcharts"
                     link="https://aerostokes.github.io/camping-starcharts/"
-                    image="http://placekitten.com/700"
+                    image="./images/Campricorn.png"
+                />
+                <Project 
+                    title="Tech Blog" 
+                    github="https://github.com/alecryanhunter/Tech-Blog" 
+                    link="https://alechunter-blog.herokuapp.com/" 
+                    image="./images/Tech-Blog.png"
                 />
                 <Project 
                     title="Quiz Game" 
-                    github="" 
-                    link="" 
-                    image="http://placekitten.com/700"
+                    github="https://github.com/alecryanhunter/Quiz-Game" 
+                    link="https://alecryanhunter.github.io/Quiz-Game/" 
+                    image="./images/Quiz-Game.png"
                 />
                 <Project 
-                    title="Quiz Game" 
-                    github="" 
-                    link="" 
-                    image="http://placekitten.com/700"
+                    title="OurSpace" 
+                    github="https://github.com/Ashlhc/OurSpace-Social-Media" 
+                    link="https://ourspace.herokuapp.com/" 
+                    image="./images/Ourspace.png"
                 />
                 <Project 
-                    title="Quiz Game" 
-                    github="" 
-                    link="" 
-                    image="http://placekitten.com/700"
+                    title="Note Taker" 
+                    github="https://github.com/alecryanhunter/Note-Taker" 
+                    link="https://arh-note-taker.herokuapp.com/" 
+                    image="./images/Note-Taker.png"
                 />
                 <Project 
-                    title="Quiz Game" 
-                    github="" 
-                    link="" 
-                    image="http://placekitten.com/700"
-                />
-                <Project 
-                    title="Quiz Game" 
-                    github="" 
-                    link="" 
-                    image="http://placekitten.com/700"
+                    title="Weather Forecast" 
+                    github="https://github.com/alecryanhunter/Weather-Forecast" 
+                    link="https://alecryanhunter.github.io/Weather-Forecast/" 
+                    image="./images/Weather-Forecast.png"
                 />
             </section>
         </section>
